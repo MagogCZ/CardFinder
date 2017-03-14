1c9f1f518f8d0377a777bc7e9512f885fe20d0ac
Tomáš Juřík <magog2@seznam.cz>
2017-03-14 21:27:48 +0100

    first magic :-)

--- CardFinder/app/src/main/java/cz/magog/cardfinder/MainActivity.java
+++ CardFinder/app/src/main/java/cz/magog/cardfinder/MainActivity.java
@@ -2,12 +2,26 @@ package cz.magog.cardfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        message = (TextView)findViewById(R.id.text_view);
+        message.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Your card is ten of hearts!", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
